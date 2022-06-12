a6a2aeeae03f15b33f89c3850e22f90174246e76
abdulazizjallow <abdulazizjallow96@gmail.com>
2022-06-12 11:39:09 +0200

    creating button elements

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 "use strict"
 const containerDiv = document.querySelector(".container");
 const btnContainer = ddocument.querySelector(".btn-container");
+const btnBlack = document.createElement("button");
+const btnGray = document.createElement("button");
+const btnRgb = document.createElement("button");
+const btnGridSize = document.createElement("button");
