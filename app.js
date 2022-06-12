d4614bfff244b2a99f1e414827409f3a5b2ea17b
abdulazizjallow <abdulazizjallow96@gmail.com>
2022-06-12 12:45:25 +0200

    creating  div elements and adding styles

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 "use strict"
-const containerDiv = document.querySelector(".container");
-const btnContainer = ddocument.querySelector(".btn-container");
+const container = document.querySelector(".container");
+const btnContainer = document.querySelector(".btn-container");
 const btnBlack = document.createElement("button");
 const btnGray = document.createElement("button");
 const btnRgb = document.createElement("button");
 const btnGridSize = document.createElement("button");
+
+function createDivs(cols, rows) {
+    for(let i = 0; i < cols * rows; i++) {
+        const div = document.createElement("div");
+        div.style.border = `1px solid red`;
+        container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        container.appendChild(div).classList.add("box");
+    }
+}
+
+createDivs(16, 16);
