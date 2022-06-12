437a26d5f346c8acbca305c942713ab21beda8fe
abdulazizjallow <abdulazizjallow96@gmail.com>
2022-06-12 22:41:38 +0200

    adding button event handlers

--- app.js
+++ app.js
@@ -17,3 +17,52 @@ function createDivs(cols, rows) {
 }
 
 createDivs(16, 16);
+
+function greyColor() {
+    const boxes = document.querySelectorAll(".box")
+    btnGray.textContent = "Gray";
+    btnGray.addEventListener("click", ()=> { 
+        boxes.forEach((box)=> 
+            box.addEventListener("mouseover", ()=> {
+                let ram = Math.floor(Math.random() * 255);
+                box.style.background = `rgb(${ram}, ${ram}, ${ram})`;
+            })
+        )
+
+    })
+    btnContainer.appendChild(btnGray).classList.add("btn");
+}
+greyColor();
+
+function blackColor() {
+    const boxes = document.querySelectorAll(".box")
+    btnBlack.textContent = "Black";
+    btnBlack.addEventListener("click", ()=> { 
+        boxes.forEach((box)=> 
+            box.addEventListener("mouseover", ()=> {
+                box.style.background = `black`;
+            })
+        )
+
+    })
+    btnContainer.appendChild(btnBlack).classList.add("btn");
+}
+blackColor();
+
+function rgbColor() {
+    const boxes = document.querySelectorAll(".box")
+    btnRgb.textContent = "Rgb";
+    btnRgb.addEventListener("click", ()=> { 
+        boxes.forEach((box)=> 
+            box.addEventListener("mouseover", ()=> {
+                let R = Math.floor(Math.random() * 255);
+                let G = Math.floor(Math.random() * 255);
+                let B = Math.floor(Math.random() * 255);
+                box.style.background = `rgb(${R}, ${G}, ${B})`;
+            })
+        )
+
+    })
+    btnContainer.appendChild(btnRgb).classList.add("btn");
+}
+rgbColor();
