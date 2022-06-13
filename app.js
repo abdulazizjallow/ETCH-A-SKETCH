e1fc1eb5cd222523e62d6cbeedc31f6e94b8b264
abdulazizjallow <abdulazizjallow96@gmail.com>
2022-06-13 12:52:30 +0200

    adding the eraser button and the resize button

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const btnBlack = document.createElement("button");
 const btnGray = document.createElement("button");
 const btnRgb = document.createElement("button");
 const btnGridSize = document.createElement("button");
+const btnEreser = document.createElement("button");
 
 function createDivs(cols, rows) {
     for(let i = 0; i < cols * rows; i++) {
@@ -66,3 +67,48 @@ function rgbColor() {
     btnContainer.appendChild(btnRgb).classList.add("btn");
 }
 rgbColor();
+
+function Ereser() {
+    const boxes = document.querySelectorAll(".box")
+    btnEreser.textContent = "Eraser";
+    btnEreser.addEventListener("click", ()=> { 
+        boxes.forEach((box)=> 
+            box.addEventListener("mouseover", ()=> {
+                box.style.background = `white`;
+            })
+        )
+
+    })
+    btnContainer.appendChild(btnEreser).classList.add("btn");
+}
+Ereser();
+
+function reset() {
+    const boxes  = document.querySelectorAll(".box");
+    boxes.forEach((box)=> box.remove());
+}
+
+function resize() {
+    btnGridSize.textContent = `Grid Size`;
+    btnGridSize.addEventListener("click", ()=> {
+        let user = prompt(`what size do you want your Grid to be`);
+        if (user === null || user < 1) {
+            reset();
+            createDivs(16, 16);
+            greyColor();
+            blackColor();
+            rgbColor();
+            Ereser();
+
+        } else {
+            reset();
+            createDivs(user, user);
+            greyColor();
+            blackColor();
+            rgbColor();
+            Ereser();
+        }
+    })
+    btnContainer.appendChild(btnGridSize).classList.add("btn");
+}
+resize();
